afe167a1dd137af4698721d43cc35388eda41ac3
João Vitor Bastos <156615917+bastosjoaovitor@users.noreply.github.com>
2024-05-14 21:53:04 -0300

    Updated buttons
    Calculation function implemented, only + and - signs still work.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,32 @@
-function insert(num) {
-    response = document.getElementById("response-text");
+function insert_number(num) {
+    var response = document.getElementById("response-text");
     response.innerText += num;
 }
 
+function insert_sinal(sinal) {
+    var response = document.getElementById("response-text");
+    response.innerText += sinal;
+}
+
+function insert_ponto() {
+    var response = document.getElementById("response-text");
+    response.innerText += '.';
+}
+
 function clean() {
-    response = document.getElementById('response-text');
+    var response = document.getElementById('response-text');
     response.innerText = '';
+}
+
+function calculate() {
+    var response = document.getElementById("response-text");
+    var expression = response.innerText;
+    var result;
+
+    try {
+        result = eval(expression);
+        response.innerText = result;
+    } catch (error) {
+        
+    }
 }
